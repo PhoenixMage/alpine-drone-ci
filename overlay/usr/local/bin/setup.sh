9ec4cf233b87dc354b8008609ac854a98223b390
Anthony Davies <anthony.t.davies@gmail.com>
2020-06-11 00:17:08 +1000

    Update to Alpine base v3.12

--- overlay/usr/local/bin/setup.sh
+++ overlay/usr/local/bin/setup.sh
@@ -6,7 +6,7 @@ release=${1:-edge}
 echo "http://dl-cdn.alpinelinux.org/alpine/$release/main" > /etc/apk/repositories
 
 apk -U upgrade -a
-apk add alpine-sdk lua-aports pigz
+apk add alpine-sdk lua-aports pigz sudo
 rm -rf /var/cache/apk/*
 
 # use buildozer for building
